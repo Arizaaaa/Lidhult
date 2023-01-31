e497a87bad7b3526dcbaf29ee9d188e06b7fc558
Andreu6 <suarezbellavistaandreu@gmail.com>
2023-01-31 20:15:12 +0100

    Componentes Login, Register y Main, creados

--- LidhultFront/src/app/app.module.ts
+++ LidhultFront/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
