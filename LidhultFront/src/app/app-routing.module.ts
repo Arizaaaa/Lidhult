a532b44407459fcda2a1ead90aa670d1361064c0
Andreu6 <suarezbellavistaandreu@gmail.com>
2023-02-01 19:12:11 +0100

    Rutes

--- LidhultFront/src/app/app-routing.module.ts
+++ LidhultFront/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login-page/login.component';
+import { MainComponent } from './pages/main-page/main.component';
+import { RegisterComponent } from './pages/register-page/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'login',
+  component: LoginComponent},
+  {path: 'register',
+  component:  RegisterComponent},
+  {path: 'main',
+  component: MainComponent}
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- LidhultFront/src/app/app.module.ts
+++ LidhultFront/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { MainComponent } from './main/main.component';
+
+import { LoginComponent } from './pages/login-page/login.component';
+import { MainComponent } from './pages/main-page/main.component';
+import { RegisterComponent } from './pages/register-page/register.component';
 
 @NgModule({
   declarations: [
